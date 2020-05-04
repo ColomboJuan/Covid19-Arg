54a15bfb9835a6587d2667847e695a29e5d917ad
Juan <juan.colombo95@gmail.com>
2020-05-04 19:50:35 -0300

    Comiit

---
+++ README.md
@@ -0,0 +1,1 @@
+COVID19-ARG-API-js
--- js/graphics.js
+++ js/graphics.js
@@ -55,6 +55,25 @@ var CasosImportados = {
   x: xLabels,
   y: xImported,
   mode: 'lines+markers',
+  name: 'Recuperados'
+};
+var TestsTotales = {
+  x: xLabels,
+  y: xTests,
+  mode: 'lines+markers',
+  name: 'Tests Totales'
+};
+var CasosRecuperados = {
+  x: xLabels,
+  y: xCasesRecovered,
+  mode: 'lines+markers',
+  name: 'Recuperados'
+};
+
+var CasosImportados = {
+  x: xLabels,
+  y: xImported,
+  mode: 'lines+markers',
   name: 'Casos Importados'
 };
 
@@ -77,12 +96,11 @@ var layout3 = {
 var layout4 = {
   title:'Grafico casos importados vs casos trasmisión comunitaria '
 };
-var config = {responsive: true}
 
-var data = [Confirmados, Activos,CasosRecuperados,CasosMuertes,config];
-var data2 = [TestConfirmados, TestsTotales,config];
-var data3 = [CasosRecuperados, CasosMuertes,config];
-var data4 = [CasosImportados, CasosTrasmisionComunitaria,config];
+var data = [Confirmados, Activos,CasosRecuperados,CasosMuertes];
+var data2 = [TestConfirmados, TestsTotales];
+var data3 = [CasosRecuperados, CasosMuertes];
+var data4 = [CasosImportados, CasosTrasmisionComunitaria];
 
 Plotly.newPlot('plot', data, layout);
 
@@ -90,39 +108,23 @@ Plotly.newPlot('plot', data, layout);
 }
 
 function getData() {
-  fetch("https://damianra.pythonanywhere.com/api/v1/alldata")
-  .then(response => response.json())
-  .then(function(data){
+fetch("https://damianra.pythonanywhere.com/api/v1/alldata")
+.then(response => response.json())
+.then(data => {
     console.log(data)
-    if (data["data"]){
-      return data;
-    }else{
-      getData();
-    }
-  })
-  .then(data => {
-      console.log(data)
-      data["data"].forEach(({date,cases,deaths,recovered,tests,communityTransmission,imported}) => {
-      xCasesConfirmed.push(cases)
-      xCasesDeaths.push(deaths)
-      xCasesRecovered.push(recovered)
-      xActives.push(cases - deaths- recovered)
-      xLabels.push(date)
-      xTests.push(tests)
-      xImported.push(imported)
-      xCommunityTransmission.push(communityTransmission)
-  
-    });
+    data["data"].forEach(({date,cases,deaths,recovered,totalTests,communityTransmission,imported}) => {
+    xCasesConfirmed.push(cases)
+    xCasesDeaths.push(deaths)
+    xCasesRecovered.push(recovered)
+    xActives.push(cases - deaths- recovered)
+    xLabels.push(date)
+    xTests.push(totalTests)
+    xImported.push(imported)
+    xCommunityTransmission.push(communityTransmission)
 
-    
-  
-    plotly()
-    SetData(xCasesConfirmed[xCasesConfirmed.length-1],xCasesDeaths[xCasesDeaths.length-1],xCasesRecovered[xCasesRecovered.length-1],xActives[xActives.length-1])
   });
-  function SetData(confirmed, deaths, recovered, actives) {
-    document.getElementById('confirmedLast').textContent = confirmed
-    document.getElementById('deathsLast').textContent = deaths
-    document.getElementById('recoveredLast').textContent = recovered
-    document.getElementById('activesLast').textContent = actives
-  }
-  }
+
+  plotly()
+});
+
+}
